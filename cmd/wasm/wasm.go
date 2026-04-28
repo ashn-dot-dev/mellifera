3f180ba10a25343d7105c050fcba05e192279068
ashn <me@ashn.dev>
2026-04-28 19:30:26 -0400

    Add top-level js library to the Wasm build
    This changeset introduces a new top-level library, js, to the Mellifera
    Wasm build base environment. This library contains the basic tools to
    interact with external JavaScript values in an FFI context. The library
    is a thin wrapper around the Go syscall/js package and is intended to
    provided low-level access to JavaScript interop tools rather than a
    high-level set of utilities. In particular, all values coming from and
    going to the JavaScript host environment are external types, and there
    is no attempt made to implicitly convert values into or from their
    external FFI representation.
    
    The top-level js library contains four entries:
    
    + js::value -> The type analogous to the js.Value struct in the
      syscall/js package. This type has five methods:
      + js::value::get(self, property) -> Gets {self}.property on a object
        as an external js::value instance.
      + js::value::set(self, property, js_value) -> Sets {self}.property on
        a object to the provided external js::value instance.
      + js::value::get_index(self, index) -> Gets {self}[index] on an object
        as an external js::value instance.
      + js::value::set_index(index, js_value) -> Sets {self}[index] on an
        object to the provided external js::value instance.
      + js::value::call(method, args) -> Invokes the method with the
        provided name using the provided argument vector of external
        js::value instances.
    + js::from_mellifera(mf_value) -> Convert a primitive Mellifera type
      into an external js::value type.
    + js::into_mellifera(js_value) -> Convert an external js::value type
      into a primitive Mellifera type.
    + js::global() -> Get the global JavaScript object (e.g. window) as an
      external js::value instance.
    
    The scope of this changeset was intentionally kept limited as a
    precaution against over-engineering. In particular, no attempt was made
    for js::from_mellifera and js::into_mellifera to automatically convert
    array object or associative-array object types, even though the
    underlying syscall/js package can perform such a conversion. Given that
    the Mellifera object model does not cleanly map to the JavaScript object
    model, any automatic conversion process would involve some assumed
    behavior that might not match programmer intention. For instance, if we
    had some JavaScript class:
    
        class Pixel {
            #r;
            #g;
            #b;
            #a;
    
            constructor(r, g, b, a = 0xff) {
                this.#r = r & 0xff;
                this.#g = g & 0xff;
                this.#b = b & 0xff;
                this.#a = a & 0xff;
            }
    
            get rgba() {
                const rBits = this.#r << 0x00;
                const gBits = this.#g << 0x08;
                const bBits = this.#b << 0x10;
                const aBits = this.#a << 0x18;
                return rBits | gBits | bBits | aBits;
            }
    
            get r() {
                return this.#r;
            }
    
            get g() {
                return this.#g;
            }
    
            get b() {
                return this.#b;
            }
    
            get a() {
                return this.#a;
            }
        }
    
    then the r, g, b, and a getters could be read in JavaScript with code of
    the form somePixel.r, and could be accessed from Mellifera with code of
    the form some_pixel.get("r"), but would not show up in the list of
    key-value pairs returned by Object.entries(somePixel). So if we were to
    convert a Pixel instance to a Mellifera map, should these properties be
    included? Should the private fields be included instead? It is not
    clear, and this low-level library does not want to be the arbiter of
    this conundrum.

--- cmd/wasm/wasm.go
+++ cmd/wasm/wasm.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"runtime/debug"
 	"syscall/js"
 
 	"ashn.dev/mellifera"
@@ -26,11 +27,244 @@ func (self textarea) Write(p []byte) (n int, err error) {
 	return len(p), nil
 }
 
+func JsValueFromMelliferaValue(ctx *mellifera.Context, mfValue mellifera.Value) (js.Value, error) {
+	switch v := mfValue.(type) {
+	case *mellifera.Null:
+		return js.Null(), nil
+	case *mellifera.Boolean:
+		return js.ValueOf(v.Data()), nil
+	case *mellifera.Number:
+		return js.ValueOf(v.Data()), nil
+	case *mellifera.String:
+		return js.ValueOf(v.Data()), nil
+	default:
+		return js.Null(), mellifera.NewError(nil, ctx.NewStringf("unable to represent Mellifera value %v in JavaScript", mfValue))
+	}
+}
+
+func JsValueIntoMelliferaValue(ctx *mellifera.Context, jsValue js.Value) (mellifera.Value, error) {
+	switch jsValue.Type() {
+	case js.TypeNull:
+		return ctx.NewNull(), nil
+	case js.TypeBoolean:
+		return ctx.NewBoolean(jsValue.Bool()), nil
+	case js.TypeNumber:
+		return ctx.NewNumber(jsValue.Float()), nil
+	case js.TypeString:
+		return ctx.NewString(jsValue.String()), nil
+	default:
+		return nil, mellifera.NewError(nil, ctx.NewStringf("unable to represent JavaScript value %v in Mellifera", jsValue))
+	}
+}
+
+func BuiltinJsFromMellifera(ctx *mellifera.Context) *mellifera.Builtin {
+	return ctx.NewBuiltin("js::from_mellifera", []mellifera.Type{
+		mellifera.TVal(mellifera.ANY),
+	}, func(ctx *mellifera.Context, arguments []mellifera.Value) (mellifera.Value, error) {
+		jsValue, err := JsValueFromMelliferaValue(ctx, arguments[0])
+		if err != nil {
+			return nil, err
+		}
+		return ctx.NewExternalWithType(JsValueType, jsValue), nil
+	})
+}
+
+func BuiltinJsIntoMellifera(ctx *mellifera.Context) *mellifera.Builtin {
+	return ctx.NewBuiltin("js::into_mellifera", []mellifera.Type{
+		mellifera.TVal(mellifera.EXTERNAL),
+	}, func(ctx *mellifera.Context, arguments []mellifera.Value) (mellifera.Value, error) {
+		jsValue, ok := arguments[0].(*mellifera.External).Data().(js.Value)
+		if !ok {
+			return nil, mellifera.NewError(nil, ctx.NewStringf("invalid JavaScript value: %v", arguments[0]))
+		}
+		return JsValueIntoMelliferaValue(ctx, jsValue)
+	})
+}
+
+var JsValueType *mellifera.Map = nil // js::value
+
+func BuiltinJsValueGet(ctx *mellifera.Context) *mellifera.Builtin {
+	return ctx.NewBuiltin("js::value::get", []mellifera.Type{
+		mellifera.TRef(mellifera.TVal(mellifera.EXTERNAL)),
+		mellifera.TVal(mellifera.STRING),
+	}, func(ctx *mellifera.Context, arguments []mellifera.Value) (mellifera.Value, error) {
+		self := arguments[0].(*mellifera.Reference)
+		delf := self.Data().(*mellifera.External)
+		property := arguments[1].(*mellifera.String)
+
+		delfJsValue, ok := delf.Data().(js.Value)
+		if !ok {
+			return nil, mellifera.NewError(nil, ctx.NewStringf("external value %v is not a JavaScript value", delf))
+		}
+
+		if delfJsValue.Type() != js.TypeObject {
+			return nil, mellifera.NewError(nil, ctx.NewStringf("external value %v is not a JavaScript object", delf))
+		}
+
+		lookup := delfJsValue.Get(property.Data())
+		return ctx.NewExternalWithType(JsValueType, lookup), nil
+	})
+}
+
+func BuiltinJsValueSet(ctx *mellifera.Context) *mellifera.Builtin {
+	return ctx.NewBuiltin("js::value::set", []mellifera.Type{
+		mellifera.TRef(mellifera.TVal(mellifera.EXTERNAL)),
+		mellifera.TVal(mellifera.STRING),
+		mellifera.TVal(mellifera.EXTERNAL),
+	}, func(ctx *mellifera.Context, arguments []mellifera.Value) (mellifera.Value, error) {
+		self := arguments[0].(*mellifera.Reference)
+		delf := self.Data().(*mellifera.External)
+		property := arguments[1].(*mellifera.String)
+		value := arguments[2].(*mellifera.External)
+
+		delfJsValue, ok := delf.Data().(js.Value)
+		if !ok {
+			return nil, mellifera.NewError(nil, ctx.NewStringf("external value %v is not a JavaScript value", delf))
+		}
+
+		if delfJsValue.Type() != js.TypeObject {
+			return nil, mellifera.NewError(nil, ctx.NewStringf("external value %v is not a JavaScript object", delf))
+		}
+
+		valueJsValue, ok := value.Data().(js.Value)
+		if !ok {
+			return nil, mellifera.NewError(nil, ctx.NewStringf("external value %v is not a JavaScript value", delf))
+		}
+
+		delfJsValue.Set(property.Data(), valueJsValue)
+		return ctx.NewNull(), nil
+	})
+}
+
+func BuiltinJsValueGetIndex(ctx *mellifera.Context) *mellifera.Builtin {
+	return ctx.NewBuiltin("js::value::get_index", []mellifera.Type{
+		mellifera.TRef(mellifera.TVal(mellifera.EXTERNAL)),
+		mellifera.TVal(mellifera.NUMBER),
+	}, func(ctx *mellifera.Context, arguments []mellifera.Value) (mellifera.Value, error) {
+		self := arguments[0].(*mellifera.Reference)
+		delf := self.Data().(*mellifera.External)
+		index := arguments[1].(*mellifera.Number)
+
+		delfJsValue, ok := delf.Data().(js.Value)
+		if !ok {
+			return nil, mellifera.NewError(nil, ctx.NewStringf("external value %v is not a JavaScript value", delf))
+		}
+
+		if delfJsValue.Type() != js.TypeObject {
+			return nil, mellifera.NewError(nil, ctx.NewStringf("external value %v is not a JavaScript object", delf))
+		}
+
+		indexInt, err := mellifera.ValueAsInt(index)
+		if err != nil {
+			return nil, mellifera.NewError(nil, ctx.NewString(err.Error()))
+		}
+
+		lookup := delfJsValue.Index(indexInt)
+		return ctx.NewExternalWithType(JsValueType, lookup), nil
+	})
+}
+
+func BuiltinJsValueSetIndex(ctx *mellifera.Context) *mellifera.Builtin {
+	return ctx.NewBuiltin("js::value::set_index", []mellifera.Type{
+		mellifera.TRef(mellifera.TVal(mellifera.EXTERNAL)),
+		mellifera.TVal(mellifera.NUMBER),
+		mellifera.TVal(mellifera.EXTERNAL),
+	}, func(ctx *mellifera.Context, arguments []mellifera.Value) (mellifera.Value, error) {
+		self := arguments[0].(*mellifera.Reference)
+		delf := self.Data().(*mellifera.External)
+		index := arguments[1].(*mellifera.Number)
+		value := arguments[2].(*mellifera.External)
+
+		delfJsValue, ok := delf.Data().(js.Value)
+		if !ok {
+			return nil, mellifera.NewError(nil, ctx.NewStringf("external value %v is not a JavaScript value", delf))
+		}
+
+		valueJsValue, ok := value.Data().(js.Value)
+		if !ok {
+			return nil, mellifera.NewError(nil, ctx.NewStringf("external value %v is not a JavaScript value", delf))
+		}
+
+		if delfJsValue.Type() != js.TypeObject {
+			return nil, mellifera.NewError(nil, ctx.NewStringf("external value %v is not a JavaScript object", delf))
+		}
+
+		indexInt, err := mellifera.ValueAsInt(index)
+		if err != nil {
+			return nil, mellifera.NewError(nil, ctx.NewString(err.Error()))
+		}
+
+		delfJsValue.SetIndex(indexInt, valueJsValue)
+		return ctx.NewNull(), nil
+	})
+}
+
+func BuiltinJsValueCall(ctx *mellifera.Context) *mellifera.Builtin {
+	return ctx.NewBuiltin("js::value::call", []mellifera.Type{
+		mellifera.TRef(mellifera.TVal(mellifera.EXTERNAL)),
+		mellifera.TVal(mellifera.STRING),
+		mellifera.TVal(mellifera.VECTOR),
+	}, func(ctx *mellifera.Context, arguments []mellifera.Value) (mellifera.Value, error) {
+		self := arguments[0].(*mellifera.Reference)
+		delf := self.Data().(*mellifera.External)
+		method := arguments[1].(*mellifera.String)
+		args := arguments[2].(*mellifera.Vector)
+
+		delfJsValue, ok := delf.Data().(js.Value)
+		if !ok {
+			return nil, mellifera.NewError(nil, ctx.NewStringf("external value %v is not a JavaScript value", delf))
+		}
+
+		argsSlice := []any{}
+		for i, arg := range args.Elements() {
+			argExternal, ok := arg.(*mellifera.External)
+			if !ok {
+				return nil, mellifera.NewError(nil, ctx.NewStringf("argument %v with value %v is not an external JavaScript value", i, arg))
+			}
+			argJsValue, ok := argExternal.Data().(js.Value)
+			if !ok {
+				return nil, mellifera.NewError(nil, ctx.NewStringf("argument %v with value %v is not an external JavaScript value", i, arg))
+			}
+			argsSlice = append(argsSlice, argJsValue)
+		}
+
+		result := delfJsValue.Call(method.Data(), argsSlice...)
+		return ctx.NewExternalWithType(JsValueType, result), nil
+	})
+}
+
+func BuiltinJsGlobal(ctx *mellifera.Context) *mellifera.Builtin {
+	return ctx.NewBuiltin("js::global", []mellifera.Type{}, func(ctx *mellifera.Context, arguments []mellifera.Value) (mellifera.Value, error) {
+		return ctx.NewExternalWithType(JsValueType, js.Global()), nil
+	})
+}
+
 func eval(source string, stdout, stderr io.Writer) (mellifera.Value, error) {
 	fmt.Printf("[mellifera.eval] stdout=%+v, stderr=%+v\n", stdout, stderr)
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Printf("[mellifera.eval] encountered panic: %v\n", r)
+			debug.PrintStack()
+		}
+	}()
+
 	ctx := mellifera.NewContext()
 	ctx.Stdout = stdout
 	ctx.Stderr = stderr
+
+	JsValueType = ctx.NewMetaMap("js::value", []mellifera.MapPair{
+		{ctx.NewString("get"), BuiltinJsValueGet(&ctx)},
+		{ctx.NewString("set"), BuiltinJsValueSet(&ctx)},
+		{ctx.NewString("get_index"), BuiltinJsValueGetIndex(&ctx)},
+		{ctx.NewString("set_index"), BuiltinJsValueSetIndex(&ctx)},
+		{ctx.NewString("call"), BuiltinJsValueCall(&ctx)},
+	}).Freeze().(*mellifera.Map)
+	ctx.BaseEnvironment.Let("js", ctx.NewMap([]mellifera.MapPair{
+		{ctx.NewString("value"), JsValueType},
+		{ctx.NewString("from_mellifera"), BuiltinJsFromMellifera(&ctx)},
+		{ctx.NewString("into_mellifera"), BuiltinJsIntoMellifera(&ctx)},
+		{ctx.NewString("global"), BuiltinJsGlobal(&ctx)},
+	}).Freeze())
 
 	lexer := mellifera.NewLexer(&ctx, source, &mellifera.SourceLocation{"<program>", 1})
 	parser, err := mellifera.NewParser(&lexer)
@@ -47,7 +281,6 @@ func eval(source string, stdout, stderr io.Writer) (mellifera.Value, error) {
 }
 
 func main() {
-	fmt.Println("Initialized Mellifera Wasm module...")
 	mf := js.Global().Get("mellifera")
 	mf.Set("eval", js.FuncOf(func(this js.Value, args []js.Value) any {
 		if len(args) == 1 {
