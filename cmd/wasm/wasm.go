f3313dfcf8af7fc3d7051221877613785559bef8
ashn <me@ashn.dev>
2026-07-31 09:16:35 -0400

    Fix Wasm panic with a zero-argument eval
    This changeset fixes a bug in the WebAssembly build where calling
    mellifera.eval() with zero arguments would panic due to a missing check
    for the zero argument case:
    
        >>> mellifera.eval();
        panic: runtime error: index out of range [0] with length 0 wasm_exec.js:22:14
        <empty string> wasm_exec.js:22:14
        goroutine 7 [running]: wasm_exec.js:22:14
        main.main.func1({{}, 0x7ff800010000000e, 0x30e098}, {0x26da60, 0x0, 0x0}) wasm_exec.js:22:14
                /Users/victorcushman/sources/mellifera/cmd/wasm/wasm.go:785 +0x57 wasm_exec.js:22:14
        syscall/js.handleEvent() wasm_exec.js:22:14
                /opt/homebrew/Cellar/go/1.26.2/libexec/src/syscall/js/func.go:117 +0x28 wasm_exec.js:22:14
        exit code: 2
    
    With this changeset, calling mellifera.eval() with zero arguments
    produces the error message one would expect to see when an invalid
    number of arguments is provided:
    
        >>> mellifera.eval();
        error: expected one or two arguments for mellifera.eval(source, options), received 0 arguments

--- cmd/wasm/wasm.go
+++ cmd/wasm/wasm.go
@@ -775,11 +775,11 @@ func BuiltinImport(ctx *mellifera.Context) mellifera.Value {
 func main() {
 	mf := js.Global().Get("mellifera")
 	mf.Set("eval", js.FuncOf(func(this js.Value, args []js.Value) any {
-		if len(args) == 1 {
-			args = append(args, js.ValueOf(map[string]any{})) // empty options
-		} else if len(args) > 2 {
+		if len(args) == 0 || len(args) > 2 {
 			fmt.Fprintf(os.Stderr, "error: expected one or two arguments for mellifera.eval(source, options), received %v arguments\n", len(args))
 			return nil
+		} else if len(args) == 1 {
+			args = append(args, js.ValueOf(map[string]any{})) // empty options
 		}
 
 		if args[0].Type() != js.TypeString {
